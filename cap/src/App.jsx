c0f5d9976d92c9120cdd676f7d917ab4155589bb
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-03-18 18:20:10 -0400

    Steps 0-1

---
+++ cap/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+import APIForm from '../components/APIForm';
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const submitForm = () => {
+    
+  }
+  
+  return (
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+
+      <APIForm
+        inputs={inputs} 
+        handleChange={(e) => 
+          setInputs((prevState) => ({
+            ...prevState, 
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+        />
+    </div>
+  );
+}
+
+export default App
